1b3e26ea195b8312b2f8dcdf314523143415220b
Lester-ozu <jlc628549@gmail.com>
2024-12-23 18:16:07 +0800

    Version 1.0.1

--- src/central/Main.java
+++ src/central/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.Scanner;
 
 public class Main extends Application{
     public static void main(String[] args) throws Exception {
